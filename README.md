4c121df0e1547a19dc24bd8c9507992592494748
Liwei <willalee0236@gmail.com>
2024-11-22 02:07:49 -0800

    modify readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This project scrapes faculty information from
 	 the [Khoury College of Computer Sciences](https://www.khoury.northeastern.edu/about/people/) at Northeastern University.
 	  It collects details including the faculty's name, title, location, research interests, and a link to their profile. 
-	  The scraped data is then saved in a CSV file for easy access.
+	  You can also download the [CSV file](https://github.com/WillaLee/facultyScraper/blob/main/faculties.csv) for easy access and analysis.
       
 # Faculty Directory
 
--- generateReadme.go
+++ generateReadme.go
@@ -19,7 +19,7 @@ func generateReadme(faculties []Faculty) error {
 	description := `This project scrapes faculty information from
 	 the [Khoury College of Computer Sciences](https://www.khoury.northeastern.edu/about/people/) at Northeastern University.
 	  It collects details including the faculty's name, title, location, research interests, and a link to their profile. 
-	  The scraped data is then saved in a CSV file for easy access.` + "\n\n"
+	  You can also download the [CSV file](https://github.com/WillaLee/facultyScraper/blob/main/faculties.csv) for easy access and analysis.` + "\n\n"
 
 	_, err = file.WriteString(description)
 	if err != nil {
